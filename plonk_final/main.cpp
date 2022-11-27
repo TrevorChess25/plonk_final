dee8ed35c1c4531a9b86cea0d1089e70756bca0c
TrevorChess25 <tmchessnoe5123@eagle.fgcu.edu>
2022-11-27 14:30:43 -0500

    Add project files.

---
+++ plonk_final/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+using namespace std;
+int main()
+{
+    sf::Window window(sf::VideoMode(720, 480), "Plonk");
+    system("pause");
+    return 0;
+}
