790c3e75573fd99813bc9fa0df05e1fadf3173f2
TrevorChess25 <tmchessnoe5123@eagle.fgcu.edu>
2022-12-05 22:18:00 -0500

    -Fixed AI paddle getting bouncing around at edges

--- plonk_final/paddle_ai.cpp
+++ plonk_final/paddle_ai.cpp
@@ -24,41 +24,13 @@ void paddle_ai::set_ball(ball* ball_obj) {
 }
 
 void paddle_ai::Update() {
-	final_paddle_pos = this->getPosition().y;
 	//shorter aliases for paddle & ball
 	ball_y_pos = this->ball_obj->getPosition().y;
 	paddle_center = this->getPosition().y + paddle_h / 2;
 
-	//alias for checking if paddle goes off-screen next update
-	paddle_near_top = this->getPosition().y + paddle_h + 7.2f > 600;
-	paddle_near_btm = this->getPosition().y - paddle_h - 7.2f < 0;
-	paddle_at_top = this->getPosition().y + paddle_h > 600;
-	paddle_at_btm = this->getPosition().y - paddle_h < 0;
-
 	//if we didn't have this paddle would move
 	//before ball was created
 	if (this->ball_obj != NULL) {
-		
-		/*
-		//Only check if paddle approaches edges when it's moving
-		if (final_paddle_pos - init_paddle_pos != 0) {
-			//when paddle is close to window's top
-			//move slowly towards top
-			if (paddle_near_top) {
-				while (!paddle_at_top) {
-					this->move(0, 1.0f);
-				}
-			}
-			//when paddle is close to window's btm
-			//move slowly towards bottom
-			if (!paddle_near_btm) {
-				while (!paddle_at_btm) {
-					this->move(0, -1.0f);
-				}
-			}
-
-		}
-		*/
 		
 		//if paddle is below ball, move up
 		if (paddle_center < ball_y_pos) {
@@ -69,7 +41,6 @@ void paddle_ai::Update() {
 		if (paddle_center > ball_y_pos) {
 			this->move(0, -7.2f);
 		}
-		init_paddle_pos = this->getPosition().y;
 	}
 
 	//calculates vertical velocity and updates via Entity's method
@@ -77,10 +48,10 @@ void paddle_ai::Update() {
 
 	//screen bottom collision
 	if (this->getPosition().y < 0) {
-		this->move(0, 20.0f);
+		this->move(0, 7.5f);
 	}
 	//screen top collision
 	if (this->getPosition().y + paddle_h > 600) {
-		this->move(0, -20.0f);
+		this->move(0, -7.5f);
 	}
 }
--- plonk_final/paddle_ai.h
+++ plonk_final/paddle_ai.h
@@ -12,7 +12,4 @@ private:
 	ball* ball_obj;
 	int player_num, ball_y_pos;
 	int paddle_h, paddle_center;
-	int init_paddle_pos, final_paddle_pos;
-	bool paddle_near_top, paddle_near_btm;
-	bool paddle_at_top, paddle_at_btm;
 };
